4a9e3f81fc2a503c680c689473778bdd28a53311
Touhidul Shawan <touhidulislamshaon@gmail.com>
2019-03-01 18:04:05 +0600

    Updates

---
+++ precache-manifest.e9c19979d9679ddb0703b8b3bffd90c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f0e2775eb364d7da44ef",
+    "url": "/simple-clock-react/static/js/runtime~main.f0e2775e.js"
+  },
+  {
+    "revision": "856b222993fbdcc7e5c0",
+    "url": "/simple-clock-react/static/js/main.856b2229.chunk.js"
+  },
+  {
+    "revision": "4e365e66681e0ac57761",
+    "url": "/simple-clock-react/static/js/2.4e365e66.chunk.js"
+  },
+  {
+    "revision": "856b222993fbdcc7e5c0",
+    "url": "/simple-clock-react/static/css/main.542cbdfc.chunk.css"
+  },
+  {
+    "revision": "4e365e66681e0ac57761",
+    "url": "/simple-clock-react/static/css/2.f5a5e51c.chunk.css"
+  },
+  {
+    "revision": "e722139e8646b98039606994560a1687",
+    "url": "/simple-clock-react/index.html"
+  }
+];
